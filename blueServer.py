826c0727c50b513551f3c43e8fab62c44385b14b
iAmTheRealJason <51766718+JasonGardner-code@users.noreply.github.com>
2024-04-01 20:51:24 -0700

    Update blueServer.py
    Bluetooth server to listen and print incoming connection

---
+++ blueServer.py
@@ -0,0 +1,27 @@
+import bluetooth
+
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("", bluetooth.PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "00001101-0000-1000-8000-00805F9B34FB"
+bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                            profiles=[bluetooth.SERIAL_PORT_PROFILE])
+
+print("Waiting for connection on RFCOMM channel", port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if not data:
+            break
+        print("Received", data.decode("utf-8"))
+finally:
+    client_sock.close()
+    server_sock.close()
